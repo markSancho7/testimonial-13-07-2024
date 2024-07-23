ecadce919b8d8b46fd7ff1a72cec3b2d7bfde9c8
markSancho7 <marcos.sancho7@alumnostrazos.net>
2024-07-23 19:16:17 +0200

    Terminado

--- src/components/containerCards/styles.js
+++ src/components/containerCards/styles.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 const StyledContainerCards = styled.div`
-	width: 1100px;
 	display: grid;
-	grid-template-columns: auto auto auto auto auto;
-	grid-template-rows: auto auto;
+	grid-template-columns: repeat(4, 270px);
+	grid-template-rows: repeat(2, 320px);
+	gap: 10px;
+	width: 1300px;
+	justify-content: center;
+	margin-inline: auto;
+	margin-top: 50px;
 `;
 
 export { StyledContainerCards };
--- src/constants/CARDSINFO.js
+++ src/constants/CARDSINFO.js
@@ -11,7 +11,7 @@ export const CARDSINFO = [
 		secondText:
 			'“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the  best - and most grueling - time of my life. Since completing the course, I’ve  successfully switched careers, working as a Software Engineer at a VR startup. ”',
 		$cardWidth: '510px',
-		$cardHeight: '282px',
+		$cardHeight: '288px',
 		$backgroundColor: '#A775F1',
 		$textColor: 'white',
 		$gridColum: '1/2',
@@ -26,10 +26,10 @@ export const CARDSINFO = [
 		secondText:
 			'“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. “',
 		$cardWidth: '255px',
-		$cardHeight: '282px',
+		$cardHeight: '288px',
 		$backgroundColor: '#48556A',
 		$textColor: 'white',
-		$gridColum: '2/3',
+		$gridColum: '3/4',
 		$gridRow: '1'
 	},
 	{
@@ -56,7 +56,7 @@ export const CARDSINFO = [
 			'Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.',
 		secondText:
 			'“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people.  ”',
-		$cardWidth: '540px',
+		$cardWidth: '535px',
 		$cardHeight: '266px',
 		$backgroundColor: '#19202D',
 		$textColor: 'white',
@@ -72,10 +72,10 @@ export const CARDSINFO = [
 		secondText:
 			'“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!  ”',
 		$cardWidth: '255px',
-		$cardHeight: '572px',
+		$cardHeight: '594px',
 		$backgroundColor: '#FFFFFF',
 		$textColor: 'black',
-		$gridColum: '3/4',
+		$gridColum: '4/4',
 		$gridRow: '1'
 	}
 ];
